26992785c6f383bfe9da31e1d30198172a50468d
Gal-N-Kubiya <galnelken07@gmail.com>
2024-10-14 23:40:02 +0300

    .

--- src/cmps/Management/PersonalDetails.jsx
+++ src/cmps/Management/PersonalDetails.jsx
@@ -6,27 +6,27 @@ const [activeWorkCount, setActiveWorkCount] = useState(getActiveWorkCount(user))
 
     return <div className="info-headers-container">
         <div className="info-header-set">
-        <h4 className="info-header-style">:שם</h4>
+        <h4 className="info-header-style">שם:</h4>
         <h4 className="info-item-style">{user.name}</h4>
         </div>
 
         <div className="info-header-set">
-        <h4 className="info-header-style">:ת"ז</h4>
+        <h4 className="info-header-style">ת"ז:</h4>
         <h4 className="info-item-style">{user.idNum}</h4>
         </div>
 
         <div className="info-header-set">
-        <h4 className="info-header-style">:טלפון</h4>
+        <h4 className="info-header-style">טלפון:</h4>
         <h4 className="info-item-style">{user.phoneNum}</h4>
         </div>
 
         <div className="info-header-set">
-        <h4 className="info-header-style">:מייל</h4>
+        <h4 className="info-header-style">מייל:</h4>
         <h4 className="info-item-style">{user.email}</h4>
         </div>
 
         <div className="info-header-set">
-        <h4 className="info-header-style">:מ"ס עבודות פתוחות</h4>
+        <h4 className="info-header-style">מ"ס עבודות פתוחות:</h4>
         <h4 className="info-item-style">{activeWorkCount}</h4>
 
         </div>
