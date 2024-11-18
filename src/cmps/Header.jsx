231c15495f8ffe36e856b0b0dad7fd90925df82e
Gal-N-Kubiya <galnelken07@gmail.com>
2024-11-18 19:28:23 +0200

    Added mark for selected header link

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom"
-
+import { Link, useLocation } from "react-router-dom"
 
 export const Header = () => {
+
+    const { pathname } = useLocation()
 
     return <header className="header-container">
         <h1 className="app-name">Sync🪠Contractor</h1>
         <nav className="header-nav-bar">
-            <Link className="link-item" to={"/login"}>התחברות</Link>
-            <Link className="link-item" to={"/permissions"}>הרשאות</Link>
-            <Link className="link-item" to={"/dashboard"}>דשבורד</Link>
-            <Link className="link-item" to={"/showWork"}>הצג עבודה</Link>
-            <Link className="link-item" to={"/managementTable"}>ניהול עבודות</Link>
-            <Link className="link-item" to='/'> בית </Link>
+            <Link className={`link-item ${pathname === "/login" ? "active-link" : ""}`} to="/login">התחברות</Link>
+            <Link className={`link-item ${pathname === "/permissions" ? "active-link" : ""}`} to="/permissions">הרשאות</Link>
+            <Link className={`link-item ${pathname === "/dashboard" ? "active-link" : ""}`} to="/dashboard">דשבורד</Link>
+            <Link className={`link-item ${pathname === "/managementTable" ? "active-link" : ""}`} to="/managementTable">ניהול עבודות</Link>
+            <Link className={`link-item ${pathname === "/" ? "active-link" : ""}`} to="/">בית</Link>
         </nav>
     </header>
 }
